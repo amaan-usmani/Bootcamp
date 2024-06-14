a801d3ff8e8939c69033dd0a44bb69aae54d0a5f
Amaan Usmani <150344153+amaan-usmani@users.noreply.github.com>
2024-06-15 01:07:39 +0530

    Create Mysql_to_python_connector.py

---
+++ Mysql_to_python_connector.py
@@ -0,0 +1,14 @@
+#Installing
+pip install mysql-python-connector
+
+#Script to connect mysql to python 
+
+import mysql.connector
+
+conn + mysql.connector.connect(host = 'localhost', username ='root', password = Usmani@9528, database = 'Apna')
+my_cursor = conn.cursor()
+
+conn.comit()
+conn.close()
+
+print("Connection succesfully created")
